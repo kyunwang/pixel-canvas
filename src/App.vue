f64e621cc11a67715f32494ac1337f492556d18d
Kangyun Wang <kevinwang8011@hotmail.com>
2018-07-28 19:12:10 +0200

    Add a static colorpicker component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
 	<div id="app">
+		<ColorPicker />
 		<Canvas />
 	</div>
 </template>
 
 <script>
 import Canvas from "./components/Canvas.vue";
+import ColorPicker from './components/ColorPicker'
 
 export default {
   name: "app",
   components: {
-    Canvas
-  }
+	 Canvas,
+	 ColorPicker,
+  },
 };
 </script>
 
